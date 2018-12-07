2aad15273fb2eaa07f29624baef5680998acb108
Qiang Li <liqiang@gmail.com>
2018-12-07 02:40:58 -0800

    reverse proxy

---
+++ rp.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+var (
+	hostProxy map[string]*httputil.ReverseProxy
+)
+
+type baseHandle struct{}
+
+func (h baseHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	host := r.Host
+
+	if fn, ok := hostProxy[host]; ok {
+		fn.ServeHTTP(w, r)
+		return
+	}
+
+	target := "http://" + host
+
+	log.Println("target: ", target)
+
+	remote, err := url.Parse(target)
+	if err != nil {
+		log.Println("target parse fail:", err)
+		return
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	hostProxy[host] = proxy
+	proxy.ServeHTTP(w, r)
+	return
+
+}
+
+func ReverseProxy(port int) {
+	hostProxy = make(map[string]*httputil.ReverseProxy)
+	h := &baseHandle{}
+	http.Handle("/", h)
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%v", port),
+		Handler: h,
+	}
+
+	//server.ListenAndServeTLS( "server.pem", "server.key")
+
+	log.Fatal(server.ListenAndServe())
+}
