a96e1c0d0658d47cca6271b47d2287e8968aff06
Qiang Li <liqiang@gmail.com>
2018-12-23 17:03:22 -0800

    tcp load balancer

---
+++ internal/lb/listener.go
@@ -0,0 +1,48 @@
+package lb
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"strconv"
+)
+
+func startServer(listenPort int, backends *Backends) {
+	port := strconv.Itoa(listenPort)
+	fmt.Println("Starting server on port ", port)
+
+	addr, _ := net.ResolveTCPAddr("tcp", ":"+port)
+
+	listener, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		fmt.Println("Could not listen on port because:", err.Error())
+		return
+	}
+
+	for {
+		con, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error occured accepting a connection", err.Error())
+		}
+
+		go handleConnection(con, backends.NextAddress())
+	}
+
+}
+
+func handleConnection(cli_conn net.Conn, srv_addr string) {
+	srv_conn, err := net.Dial("tcp", srv_addr)
+	if err != nil {
+		fmt.Printf("Could not connect to server (%q), connection dropping\n", srv_addr)
+		return
+	}
+
+	// close the conections when done
+	defer func() {
+		srv_conn.Close()
+		cli_conn.Close()
+	}()
+
+	go io.Copy(cli_conn, srv_conn)
+	io.Copy(srv_conn, cli_conn)
+}
---
+++ internal/lb/server.go
@@ -0,0 +1,68 @@
+//https://github.com/voldyman/GoLoadBalance
+package lb
+
+import (
+	//"flag"
+	"fmt"
+	"runtime"
+	"time"
+)
+
+// func main() {
+// 	var backends_input strings
+
+// 	var port = flag.Int("port", 8050, "The port to listen for connections")
+// 	var debug = flag.Bool("debug", false, "Enable debug mode")
+
+// 	flag.Var(&backends_input, "backend", "List of backend servers, need at least one")
+
+// 	flag.Parse()
+
+// 	if flag.NFlag() == 0 || len(backends_input) < 1 {
+// 		flag.PrintDefaults()
+// 		return
+// 	}
+
+// 	backends := NewBackends()
+
+// 	backends.Add([]string(backends_input)...)
+
+// 	if *debug {
+// 		go debugRoutine()
+// 	}
+
+// 	startServer(*port, backends)
+// }
+
+// Start starts load balancer
+func Start(port int, backends []string, debug bool) {
+	be := NewBackends()
+
+	be.Add([]string(backends)...)
+
+	if debug {
+		go debugRoutine()
+	}
+
+	startServer(port, be)
+}
+
+func debugRoutine() {
+	for {
+		<-time.After(2 * time.Second)
+		fmt.Println(time.Now(), "NumGoroutine", runtime.NumGoroutine())
+	}
+}
+
+// type strings []string
+
+// func (self *strings) String() string {
+// 	return fmt.Sprintf("%s", *self)
+// }
+
+// func (self *strings) Set(val string) error {
+
+// 	*self = append(*self, val)
+
+// 	return nil
+// }
