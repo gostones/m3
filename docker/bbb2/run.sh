09cf9272f746094a1872824463477dc4832581cc
Qiang Li <liqiang@gmail.com>
2019-01-29 20:19:05 -0800

    bbb dockers

---
+++ docker/bbb2/run.sh
@@ -0,0 +1,15 @@
+
+#
+#export TAG_PREFIX=fcecagno/bigbluebutton:
+#export TAG_SUFFIX=
+export SERVER_DOMAIN=bbb.home
+export SCREENSHARE_EXTENSION_KEY=
+export SCREENSHARE_EXTENSION_LINK=
+export SHARED_SECRET=
+export COTURN_REST_SECRET=
+export EXTERNAL_IP=localhost
+export SECRET_KEY_BASE=
+
+docker-compose $@
+
+
