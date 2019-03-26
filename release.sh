a9d01f9fca3540e5f8c490a77dd4761d9553a6c3
Qiang Li <liqiang@gmail.com>
2019-03-26 08:04:09 -0700

    goreleaser

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -x
+
+cmds=(
+gpm
+gsh
+hexid
+m3
+mirr
+)
+
+mkdir build
+for i in "${cmds[@]}"; do
+    export APP_NAME=${i}
+    envsubst '$APP_NAME' < goreleaser-tpl.yml > build/${APP_NAME}.goreleaser.yml
+    goreleaser --config=build/${APP_NAME}.goreleaser.yml $@
+done
